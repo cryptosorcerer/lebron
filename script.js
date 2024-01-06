c5ac09c80e558be8d5446cf58239330bb1aaccdd
cryptosorcerer <155659151+cryptosorcerer@users.noreply.github.com>
2024-01-05 21:12:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-function redirectTo(url) {
-    window.open(url, '_blank');
+function placeCows() {
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+
+    document.querySelectorAll('.cow').forEach(function(cow) {
+        var randomX = Math.floor(Math.random() * (viewportWidth - 200));
+        var randomY = Math.floor(Math.random() * (viewportHeight - 200));
+
+        cow.style.position = 'absolute';
+        cow.style.left = randomX + 'px';
+        cow.style.top = randomY + 'px';
+    });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const mysticText = document.getElementById('m'); // Replace with your actual element ID for $MYSTIC
-    const imageElement = document.getElementById('q'); // Replace with your actual image element ID
-    const buttons = document.getElementById('b');
+window.onload = placeCows;
+window.onresize = placeCows; // Reposition cows when window is resized
+document.addEventListener('DOMContentLoaded', function() {
+    var container = document.getElementById('textContainer');
+    var containerWidth = window.innerWidth;
+    var containerHeight = window.innerHeight;
 
-    let mouseMoveTimeout;
+    for (var i = 0; i < 50; i++) { // Adjust the number of texts as needed
+        var text = document.createElement('div');
+        text.className = 'textItem';
+        text.textContent = '$LeBronAliens2024';
+        var x = Math.random() * containerWidth;
+        var y = Math.random() * containerHeight;
+        var rotation = Math.random() * 360;
 
-    document.addEventListener('mousemove', function() {
-        mysticText.classList.add('vibrating');
-        imageElement.classList.add('vibrating');
-        buttons.classList.add('vibrating');
+        text.style.left = x + 'px';
+        text.style.top = y + 'px';
+        text.style.transform = 'rotate(' + rotation + 'deg)';
 
-        clearTimeout(mouseMoveTimeout);
-        mouseMoveTimeout = setTimeout(() => {
-            mysticText.classList.remove('vibrating');
-            imageElement.classList.remove('vibrating');
-            buttons.classList.remove('vibrating');
-        }, 100); // Time in milliseconds; adjust as necessary
-    });
-
-    // ... your existing JavaScript code ...
+        container.appendChild(text);
+    }
 });
-
-
-
